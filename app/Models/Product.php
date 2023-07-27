f4a160f7a020dda7537a70190e94acac001462e9
prog2608 <nurygurban01@gmail.com>
2023-07-28 04:43:53 +0500

    1.1.0

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -14,6 +14,101 @@ class Product extends Model
     ];
 
     protected $casts = [
-        ''
-    ]
+        'discount_start' => 'datetime',
+        'discount_end' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',   ''
+    ];
+
+    protected static function booted()
+    {
+        static ::creating(function($obj){
+            $obj->credit = $obj->price >= 500?1:0;
+        });
+        static::saving(function($obj){
+            $obj->slug = str()->slug($obj->full_name_tm).'-'. $obj->id;
+        });
+    }
+
+    public function user()
+    {
+        return $this ->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this ->belongsTo(Category::class);
+    }
+
+    public function brand()
+    {
+        return $this ->belongsTo(Brand::class);
+    }
+
+    public function values()
+    {
+        return $this->belongsToMany(AttributeValue::class,'product_attribute_value');
+    }
+
+    public function isDiscount()
+    {
+        if($this->discount_percent > 0 and $this->discount_start <= Carbon::now()->toDateTimeString() and $this->discount_end >= Carbon::now()->toDateTimeString()){
+            return true;
+        }else{
+            return false ;
+        }
+    }
+
+    public function getDiscountPercent()
+    {
+        if($this->isDiscount()){
+            return $this ->discount_percent;
+        }else{
+            return 0;
+        }
+    }
+
+    public function getPrice()
+    {
+        return round($this ->price*(1- $this->getDiscountPercent() / 100), 1);
+    }
+
+    public function isOwner()
+    {
+        if($this->user_id == auth()->id() or auth()->user()->isAdmin()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public function getFullName()
+    {
+        $Locale = app()->getLocale();
+        switch($locale){
+            case'tm':
+                return $this->full_name_tm;
+                break;
+            case'en':
+                return $this->full_name_en ?: $this->full_name_tm;
+                break;
+            default:
+                return $this->full_name_tm;
+        }
+    }
+
+    public function getName()
+    {
+        $Locale = app()->getLocale();
+        switch ($Locale){
+            case'tm':
+                return $this->name_tm;
+                break;
+            case'en':
+                return $this->name_en ?: $this->name_tm;
+                break;
+            default:
+                return $this->name_tm;
+        }
+    }
 }
