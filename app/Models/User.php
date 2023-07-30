6fc67d52ba35cde73c26ce7015a6d14d6afcb247
prog2608 <nurygurban01@gmail.com>
2023-07-30 21:32:29 +0500

    1.2.0

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,11 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+ use Illuminate\Contracts\Auth\MustVerifyEmail;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -23,21 +19,20 @@ class User extends Authenticatable
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    public $timestamps = false;
+
+    public function products()
+    {
+        return $this ->hasMany(Product::class)
+    }
+
+
+
     protected $casts = [
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
